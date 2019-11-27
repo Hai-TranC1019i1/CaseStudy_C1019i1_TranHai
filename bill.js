446a9ccd2f43390ab544da2b33d949b31b9ba958
Hai-TranC1019i1 <tranhaicoder@gmail.com>
2019-11-27 14:13:58 +0700

    first time up file

---
+++ bill.js
@@ -0,0 +1,45 @@
+let Bill = function () {
+    this.id = '';
+
+    this.addBill = function (idTable1, idTable2) {
+        this.Bill = this.Bill(idTable1) + this.Bill(idTable2);
+    };
+    this.payAndPrintBill = function (index) {
+        let bill = '<p><strong><br>MilkTea & Coffee {CODE}</strong></p>';
+        let price = 0;
+        let date = new Date();
+        let amount = 0;
+        this.id = "b" + stt + date.valueOf();
+        bill += "<p><strong>&nbsp;&nbsp;&nbsp;while(alive){sleep(); code(); eat();}</strong></p>"
+        bill += "<p>Date: " + date.toDateString() + "</p>";
+        bill += "<p>CodeBill: " + this.id + "</p>";
+        bill += '<hr><br>';
+        bill += "<table><tr><th>Name Of Drink</th><th>Amount</th><th>Prices</th></tr>";
+        for (let i = 0; i < ListDrinking.length; i++)
+            if (ListTable[index].TypeOfDrinkAndAmout[i] != 0) {
+                amount = ListTable[index].TypeOfDrinkAndAmout[i];
+                bill += "<tr><td>" + ListDrinking[i].name + "</td><td>" + amount + "</td>" +
+                    "<td>" + amount * ListDrinking[i].price + ".000 VND</td></tr>"
+                price += amount * ListDrinking[i].price;
+        }
+        bill += "<tr><td>Total:</td><td></td><td>" + price + ".000 VND</td></tr>" +
+            "<tr><td><input type='button' value='Home' onclick='displayMainApp()'>" +
+            "</td><td><input type='button' value='EmptyTable' onclick='emptyTable(" + index + ")'></td></tr></table>";
+        display.innerHTML = bill;
+        stt++;
+    };
+}
+
+let stt = 0;
+
+let bill1 = new Bill();
+let bill2 = new Bill();
+let bill3 = new Bill();
+let bill4 = new Bill();
+let bill5 = new Bill();
+let bill6 = new Bill();
+let bill7 = new Bill();
+let bill8 = new Bill();
+let bill9 = new Bill();
+
+let ListBill = [bill1, bill2, bill3, bill4, bill5, bill6, bill7, bill8, bill9];
---
+++ initializationAndDirectional.js
@@ -0,0 +1,102 @@
+function clickAdd(indexDrink, indexTable) {
+    let idAmount = 'amountDrinking' + indexDrink;
+    ListTable[indexTable].TypeOfDrinkAndAmout[indexDrink] = document.getElementById(idAmount).value;
+    ListTable[indexTable].addAmount(indexDrink);
+    document.getElementById(idAmount).value = ListTable[indexTable].TypeOfDrinkAndAmout[indexDrink];
+};
+
+function clickSub(indexDrink, indexTable) {
+    let idAmount = 'amountDrinking' + indexDrink;
+    if (ListTable[indexTable].TypeOfDrinkAndAmout[indexDrink] > 0) {
+        ListTable[indexTable].TypeOfDrinkAndAmout[indexDrink] = document.getElementById(idAmount).value;
+        ListTable[indexTable].subAmount(indexDrink);
+        document.getElementById(idAmount).value = ListTable[indexTable].TypeOfDrinkAndAmout[indexDrink];
+    }
+    ;
+};
+
+function payBill(index) {
+    for (let i = 0; i < ListDrinking.length; i++)
+        if (ListTable[index].TypeOfDrinkAndAmout[i] != 0) {
+            ListBill[index].payAndPrintBill(index);
+            ListTable[index].havePaid = true;
+            ListTable[index].isEmty = false;
+            return;
+        }
+}
+
+function emptyTable(index) {
+    ListTable[index].isEmty = true;
+    ListTable[index].havePaid = false;
+    ListTable[index].resetTable();
+    displayMainApp();
+}
+
+function saveAndBackHome(index) {
+    for (let i = 0; i < ListDrinking.length; i++)
+        if (ListTable[index].TypeOfDrinkAndAmout[i] != 0) {
+            ListTable[index].isEmty = false;
+            break;
+        }
+    displayMainApp();
+};
+
+function displayDrinking(indexTable) {
+    let drawListDrinking = "<table width='1300' height='600'>";
+    let k = 0;
+    ListTable[indexTable].resetTable();
+    for (let i = 0; i < 2; i++) {
+        drawListDrinking += "<tr>"
+        for (let j = 0; j < 4; j++) {
+            drawListDrinking += "<td>" +
+                "<img src=" + "'images/" + ListDrinking[k].name + ".jpg" + "' width='300' height='300'" +
+                "onclick='clickAdd(" + k + "," + indexTable + ")'><br>" +
+                "<p id='nameDrinking'>" + ListDrinking[k].name + "</p>" +
+                "<p>Price: " + ListDrinking[k].price + ".000 VND</p>" +
+                "<span id='amount'>Amount: </span>"
+                + "<button id='subDrinking' onclick='clickSub(" + k + "," + indexTable + ")'>-</button><input type='number' value='" +
+                ListTable[indexTable].TypeOfDrinkAndAmout[k] + "' id='amountDrinking" + k + "'>" +
+                "<button id='addDrinking' onclick='clickAdd(" + k + "," + indexTable + ")'>+</button><br>" + "</td>";
+            k++;
+        }
+        ;
+        drawListDrinking += "</tr>";
+    }
+    ;
+    drawListDrinking += "<tr><td colspan='4'><br><input onclick='saveAndBackHome(" + indexTable + ")' value='Order' " +
+        "type='button'><input onclick='payBill(" + indexTable + ")' value='Pay' type='button'>" +
+        "<input type='button' value='EmptyTable' onclick='emptyTable(" + indexTable + ")'><br></td></tr></table>";
+    display.innerHTML = drawListDrinking;
+    ListTable[indexTable].resetTable();
+};
+
+function displayMainApp() {
+    let drawTable = "<table width='1300' height='600'>";
+    let k = 0;
+    for (let i = 0; i < 3; i++) {
+        drawTable += "<tr>";
+        for (let j = 0; j < 3; j++) {
+            drawTable += "<td style='border:1px solid #000000; text-align: center; ; background-color: " +
+                ListTable[k].colorStt() + "' " + "onclick= 'displayDrinking(" + k + ")'>Table " + k + "</td>"
+            k++;
+        }
+        ;
+        drawTable += "</tr>";
+    }
+    ;
+    drawTable += "</table>";
+    display.innerHTML = drawTable;
+};
+
+function displayLogo() {
+    let logo = "<img src='images/logo.jpg' width='1340' height='630' onclick='displayMainApp()'>";
+    display.innerHTML = logo;
+};
+
+function init() {
+    this.displayLogo();
+};
+
+let display = document.getElementById('displayApp');
+
+init();
